fe336fca4f6a92b661bca79d47ac1fcf6fba8514
Ron <rshaeffer@gmail.com>
2014-11-03 08:45:59 -0800

    junk

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         println("hello")
         println("goodbye")
+        println("junk")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
